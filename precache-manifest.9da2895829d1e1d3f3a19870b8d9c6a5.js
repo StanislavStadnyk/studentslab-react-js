87e0ea09aa9cde9a62019114d5e8c98e006f91bd
StanislavStadnyk <stanislav.stadnyk@gmail.com>
2020-05-29 10:22:55 +0300

    Updates

---
+++ precache-manifest.9da2895829d1e1d3f3a19870b8d9c6a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c512f7c0ea40fcd6396c81bb9a820c",
+    "url": "/studentslab-react-js/index.html"
+  },
+  {
+    "revision": "fff7e4aa3651fe36e283",
+    "url": "/studentslab-react-js/static/css/2.b211811c.chunk.css"
+  },
+  {
+    "revision": "2ac693a4fe6a0317fd67",
+    "url": "/studentslab-react-js/static/css/main.cefeae20.chunk.css"
+  },
+  {
+    "revision": "fff7e4aa3651fe36e283",
+    "url": "/studentslab-react-js/static/js/2.2245908a.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/studentslab-react-js/static/js/2.2245908a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ac693a4fe6a0317fd67",
+    "url": "/studentslab-react-js/static/js/main.0bc83380.chunk.js"
+  },
+  {
+    "revision": "311af3d144b8e5831d68",
+    "url": "/studentslab-react-js/static/js/runtime-main.66b12cfb.js"
+  }
+]);
